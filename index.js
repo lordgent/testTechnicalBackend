65d66a9d02a1b356bdd9f4b6a3c2446d489d692c
lordgent <lorjenx08@gmail.com>
2022-01-10 14:25:22 +0700

    setup port in index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const port = process.env.PORT || 4005;
+const app = express();
+const router = require("./src/routes/index");
+app.get("/", (req, res) => {
+  res.send("Welcome my rest api");
+});
+app.get("/restapi/v1", router);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
